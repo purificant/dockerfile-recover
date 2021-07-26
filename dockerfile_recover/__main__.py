eecc87d86154cb55c0eaf6c32b243e3a15095d7d
purificant <purificant@users.noreply.github.com>
2021-07-26 21:04:11 +0100

    add --pull option and handle errors

--- dockerfile_recover/__main__.py
+++ dockerfile_recover/__main__.py
@@ -2,30 +2,48 @@
 
 import click
 import docker
-from docker.errors import ImageNotFound
+from docker.errors import APIError, ImageNotFound
 from docker.models.images import Image
 
 from dockerfile_recover.parser import parse_history
 
 
 @click.command(
-    help="Recover Dockerfile by providing an image name similar to: docker pull <name>"
+    help="Recover Dockerfile given an image name. Will search `docker images` to find the image, or can optionally --pull it first."
+)
+@click.option(
+    "--pull",
+    is_flag=True,
+    default=False,
+    help="Pull the image first. Default is to search for image in `docker images` without pulling.",
 )
 @click.argument("name")
-def main(name):
+def main(name: str, pull: bool) -> None:
     """Command line interface entrypoint."""
 
-    client = docker.from_env()
+    docker_client = docker.from_env()
 
     try:
-        client.images.pull(name)
-    except ImageNotFound:
-        click.echo(f"{ImageNotFound.__name__}: {name}", err=True)
+        if pull:
+            click.echo(f"Pulling image {name}")
+            docker_client.images.pull(name)
+
+        image: Image = docker_client.images.get(name)
+    except ImageNotFound as image_not_found:
+        click.echo(f"{ImageNotFound.__name__}: {image_not_found}", err=True)
+        if not pull:
+            click.echo(f"Did you want to --pull the image?")
+        return
+    except APIError as api_error:
+        click.echo(f"{APIError.__name__}: {api_error}", err=True)
         return
 
-    image: Image = client.images.get(name)
+    try:
+        history = image.history()
+    except APIError as api_error:
+        click.echo(f"{APIError.__name__}: {api_error}", err=True)
+        return
 
-    history = image.history()
     history.reverse()
 
     lines = parse_history(history)
@@ -33,6 +51,7 @@ def main(name):
     click.echo("")
     for line in lines:
         click.echo(line)
+
 
 if __name__ == "__main__":
     main()
